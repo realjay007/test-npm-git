6f0b146fc324e0a3d93eded39cdb56e2e871e629
Julius Ijie <juliusijie@gmail.com>
2019-03-09 22:35:11 +0100

    Document greet function Add new checkUpon function

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,13 @@
-export function greet(name: string = "there") {
-	return `Hello ${name}`;
+/**
+ * Return a greeting
+ * @param name Name of person
+ */
+export function greet(name: string = "there"): string {
+	return `Hello ${name}!`;
+}
+
+/** See how the person is doing */
+export function checkUpOn(): string {
+	return "How are you doing today?";
 }
 
